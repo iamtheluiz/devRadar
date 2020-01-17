b4efd62d519f1dabb01ea0476909a525f9233a78
iamtheluiz <iamtheluiz.dev@gmail.com>
2020-01-17 20:11:29 -0300

    Backend: Add MongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,13 @@
 const express = require('express'); // Import express (server base)
+const mongoose = require('mongoose'); // Connection with MongoDB
 
+const port = 3333;  // Application port
 const app = express();  // Start app
+
+mongoose.connect('mongodb+srv://devRadar:5KeYF1o0VnLZ0nCZ@cluster0-msvvd.mongodb.net/devRadar?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}); // Connect to MongoDB Atlas
 
 // Accept JSON requisitions
 app.use(express.json());
@@ -11,4 +18,6 @@ app.get('/', (req, res) => {
 });
 
 // Define app port
-app.listen(3333);
+app.listen(port);
+
+console.log(`Application listening to localhost:${port}`);
