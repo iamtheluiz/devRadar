e5c9ebc99c4c1cd1a069209b75c9fdb3c6ca91fb
iamtheluiz <iamtheluiz.dev@gmail.com>
2020-01-19 14:50:31 -0300

    Web: Add dev list

--- web/src/App.js
+++ web/src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import './global.css';
 import './App.css';
 import './Sidebar.css';
+import './Main.css';
 
 function App() {
   return (
@@ -34,8 +35,54 @@ function App() {
           <button type="submit">Salvar</button>
         </form>
       </aside>
-      <main>
 
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/32724000?v=4" alt="Luiz Gustavo" />
+              <div className="user-info">
+                <strong>Luiz Gustavo</strong>
+                <span>ReactJS, NodeJS, PHP</span>
+              </div>
+            </header>
+            <p>Um Técnico em Informática para Internet entusiasmado com a tecnologia.</p>
+            <a href="https://github.com/iamtheluiz">Acessar perfil do Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/32724000?v=4" alt="Luiz Gustavo" />
+              <div className="user-info">
+                <strong>Luiz Gustavo</strong>
+                <span>ReactJS, NodeJS, PHP</span>
+              </div>
+            </header>
+            <p>Um Técnico em Informática para Internet entusiasmado com a tecnologia.</p>
+            <a href="https://github.com/iamtheluiz">Acessar perfil do Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/32724000?v=4" alt="Luiz Gustavo" />
+              <div className="user-info">
+                <strong>Luiz Gustavo</strong>
+                <span>ReactJS, NodeJS, PHP</span>
+              </div>
+            </header>
+            <p>Um Técnico em Informática para Internet entusiasmado com a tecnologia.</p>
+            <a href="https://github.com/iamtheluiz">Acessar perfil do Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/32724000?v=4" alt="Luiz Gustavo" />
+              <div className="user-info">
+                <strong>Luiz Gustavo</strong>
+                <span>ReactJS, NodeJS, PHP</span>
+              </div>
+            </header>
+            <p>Um Técnico em Informática para Internet entusiasmado com a tecnologia.</p>
+            <a href="https://github.com/iamtheluiz">Acessar perfil do Github</a>
+          </li>
+        </ul>
       </main>
     </div>
   )
