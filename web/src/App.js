c1ff90e918daade78a5ba09f25673adb1ffb3602
iamtheluiz <iamtheluiz.dev@gmail.com>
2020-01-19 15:56:55 -0300

    Web: Add dev and list devs with api

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import api from './services/api.js';
 
 // Styles
 import './global.css';
@@ -7,29 +9,105 @@ import './Sidebar.css';
 import './Main.css';
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  const [github_username, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (error) => {
+        console.log(error);
+      },
+      {
+        timeout: 30000,
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, [])
+
+  async function handleAddDev(event) {
+    event.preventDefault();
+
+    const response = await api.post('/devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude
+    });
+
+    setGithubUsername('');
+    setTechs('');
+
+    setDevs([...devs, response.data]);
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
+        <form onSubmit={handleAddDev}>
           <div className="input-block">
             <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required />
+            <input
+              name="github_username"
+              id="github_username"
+              required
+              value={github_username}
+              onChange={event => setGithubUsername(event.target.value)}
+            />
           </div>
 
           <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
+            <input
+              name="techs"
+              id="techs"
+              required
+              value={techs}
+              onChange={event => setTechs(event.target.value)}
+            />
           </div>
 
           <div className="input-group">
             <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
+              <input
+                type="number"
+                name="latitude"
+                id="latitude"
+                required
+                value={latitude}
+                onChange={event => setLatitude(event.target.value)}
+              />
             </div>
             <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
+              <input
+                type="number"
+                name="longitude"
+                id="longitude"
+                required
+                value={longitude}
+                onChange={event => setLongitude(event.target.value)}
+              />
             </div>
           </div>
           <button type="submit">Salvar</button>
@@ -38,50 +116,19 @@ function App() {
 
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/32724000?v=4" alt="Luiz Gustavo" />
-              <div className="user-info">
-                <strong>Luiz Gustavo</strong>
-                <span>ReactJS, NodeJS, PHP</span>
-              </div>
-            </header>
-            <p>Um Técnico em Informática para Internet entusiasmado com a tecnologia.</p>
-            <a href="https://github.com/iamtheluiz">Acessar perfil do Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/32724000?v=4" alt="Luiz Gustavo" />
-              <div className="user-info">
-                <strong>Luiz Gustavo</strong>
-                <span>ReactJS, NodeJS, PHP</span>
-              </div>
-            </header>
-            <p>Um Técnico em Informática para Internet entusiasmado com a tecnologia.</p>
-            <a href="https://github.com/iamtheluiz">Acessar perfil do Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/32724000?v=4" alt="Luiz Gustavo" />
-              <div className="user-info">
-                <strong>Luiz Gustavo</strong>
-                <span>ReactJS, NodeJS, PHP</span>
-              </div>
-            </header>
-            <p>Um Técnico em Informática para Internet entusiasmado com a tecnologia.</p>
-            <a href="https://github.com/iamtheluiz">Acessar perfil do Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/32724000?v=4" alt="Luiz Gustavo" />
-              <div className="user-info">
-                <strong>Luiz Gustavo</strong>
-                <span>ReactJS, NodeJS, PHP</span>
-              </div>
-            </header>
-            <p>Um Técnico em Informática para Internet entusiasmado com a tecnologia.</p>
-            <a href="https://github.com/iamtheluiz">Acessar perfil do Github</a>
-          </li>
+          {devs.map(dev => (
+            <li className="dev-item" key={dev._id}>
+              <header>
+                <img src={dev.avatar_url} alt={dev.name} />
+                <div className="user-info">
+                  <strong>{dev.name}</strong>
+                  <span>{dev.techs.join(', ')}</span>
+                </div>
+              </header>
+              <p>{dev.bio}</p>
+              <a href={`https://github.com/${dev.github_username}`}>Acessar perfil do Github</a>
+            </li>
+          ))}
         </ul>
       </main>
     </div>
